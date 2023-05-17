8e67cb78cc10e3928167189a89ab7480ed80ac48
Matvii Kharchenko <matviy.kharchenko@gmail.com>
2023-05-17 14:07:18 +0200

    bug from Belarus -> Prague

--- src/dijkstra_2/core.clj
+++ src/dijkstra_2/core.clj
@@ -42,6 +42,12 @@
 (load-file "/Users/matvii_kharchenko/Desktop/PCU/ALDS/assignment2/e-roads-2020-full.clj")
 
 
+(defn graph-reset [graph]
+  (doseq [vertex (vals @(:verticies graph))]
+    (dosync (ref-set (:status vertex) (ref 0))
+            (ref-set (:distance vertex) (ref ##Inf))
+            (ref-set (:neighbors vertex) (ref '())))))
+
 (defn vertex-unseen? [vertex]
   (= @(:status vertex) 0))
 
@@ -103,8 +109,10 @@
                            (- currentDistance neighborWeight) currentNeighborDistance)]
           (println "neighborWeight" neighborWeight "\n"
                    "currentDistance" currentDistance "\n"
-                   "currentDistance - neighborWeight"
+                   "currentDistance - neighborWeight" "\n"
                    (- currentDistance neighborWeight) "\n"
+                   "\n"
+                   "isMoveValid" isMoveValid "\n"
                    "currentNeighborDistance" currentNeighborDistance "\n"
                    "currentLabel" currentLabel "\n\n" "*********")
 
@@ -113,6 +121,7 @@
             (recur (rest neighbors) best-distance best-label)))))))
 
 (defn dijsktra-weights [from to graph]
+  ;; (graph-reset graph)
   (graph-bfs! graph to)
   (loop [from from
          places (list from)]
@@ -120,19 +129,85 @@
       (println  "------ closestNeighbor" closestNeighbor "------")
       (if (nil? closestNeighbor) "There is no path"
           (if (= closestNeighbor to)
-            (doseq [val (reverse (conj places to))] (println val))
+            (loop [placesList (reverse (conj places to))
+                   index 1]
+              (when (not (empty? placesList))
+                (let [currentPlace (first placesList)
+                      distance @(:distance (get @(:verticies g) currentPlace))]
+                  (println (str index ". " distance "km " currentPlace))
+                  (recur (rest placesList) (inc index)))))
             (recur closestNeighbor
                    (conj places closestNeighbor)))))))
 
-(get @(:verticies g) "Brest, Belarus")
-
-(defn print-distance
-  [label]
-  (println "Distance of" label "is " @(:distance (get @(:verticies g) label))
-))
-
-(get @(:verticies g) "X: E85 x E30 (0)")
-(print-distance "X: E85 x E30 (0)")
-
-(dijsktra-weights "Oskemen" "Prague" g)
+(dijsktra-weights "Manchester" "Dover" g)
 (dijsktra-weights "Brest, Belarus" "Prague" g)
+
+(graph-bfs! g "Dover")
+
+(defn visited? [vertex]
+  (not (= @(:status vertex) 0)))
+
+(defn status-color [s]
+  (case s
+    0 "#ffffff"
+    1 "#01afd1"
+    2 "#f8e71c"
+    3 "#e6e6e6"
+    "#ff0000"))
+
+;; TODO: status colors
+(defn graph-print-dot
+  ([graph]
+   (graph-print-dot graph (fn [v] true)))
+  ([graph pred?]
+   (graph-print-dot graph pred? false))
+  ([graph pred? inverted?]
+   (let [vertices (doall (map-indexed
+                          (fn [idx itm] [idx itm])
+                          (filter pred? (vals @(:verticies graph)))))
+         l2i (apply hash-map (flatten (doall (map
+                                              (fn [p] [(:label (second
+                                                                p)) (first p)]) vertices))))]
+     (println "graph G{")
+     (doseq [vertexpair vertices]
+       (let [idx (first vertexpair)
+             vertex (second vertexpair)
+             lat (:lat vertex)
+             lon (:lon vertex)]
+         (println (str "v" idx
+                       "[label=\"{"
+                       (:label vertex)
+                        ;;"|status: " @(:status vertex)
+                       "|distance: " (or @(:distance vertex) "--")
+                      ;;  "|estimate: " (if @(:estimate vertex)
+                      ;;                  (int (Math/floor @(:estimate
+                      ;;                                     vertex)))
+                      ;;                  "--")
+                       "}\""
+                       ",shape=record,style=filled,fillcolor=\""
+                       (status-color @(:status vertex))
+                       "\",pos=\""
+                       (if inverted? lat lon)
+                       ","
+                       (if inverted? lon lat)
+                       "\""
+                       "]"))))
+     (let [edges (filter
+                  (fn [a]
+                    (< (first a) (second a)))
+                  (apply
+                   concat
+                   (for [vertex vertices]
+                     (map (fn [n]
+                            [(first vertex)
+                             (get l2i (:label n))
+                             (:weight n)
+                             (:road n)])
+                          @(:neighbors (second vertex))))))]
+       (doseq [edge edges]
+         (println (str "v" (first edge) " -- v" (second edge)
+                       " [label=\"" (nth edge 3) ", " (nth edge 2) "km\"]"))))
+     (println "}"))))
+
+(graph-print-dot g visited?)
+
